dfb8adf50390c5700b5c75d01868ca6c4aec4695
shirley Lin <348475822@qq.com>
2020-06-21 22:59:53 +0800

    ok

--- 02vue/09 项目实战/mms/jinfeng-mms/src/router/index.js
+++ 02vue/09 项目实战/mms/jinfeng-mms/src/router/index.js
@@ -1,16 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Login from '@/views/login';//就相当于 ./src/views/login/index.vue ;如果你的组件名就是index.vue可以忽略不写
+import Login from '@/views/login'; //就相当于 ./src/views/login/index.vue ;如果你的组件名就是index.vue可以忽略不写
+
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    component: Login //路由用到的组件，我都统一放在views里面
-  }
-];
+const routes = [{
+  path: '/login',
+  name: 'login',
+  component: Login //路由用到的组件，我都统一放在views里面
+}];
 
 const router = new VueRouter({
   // mode: "history",
