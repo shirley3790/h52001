2ebc9c346c80bd0644f02af8d55cb829fe951c1f
shirley <348475822@qq.com>
2020-06-23 15:33:23 +0800

    会员管理

--- 02vue/09 项目实战/mms/jinfeng-mms/src/views/member/index.vue
+++ 02vue/09 项目实战/mms/jinfeng-mms/src/views/member/index.vue
@@ -1,9 +1,47 @@
 <template>
   <div>
+    <!-- 表单组件 -->
+    <el-form
+      ref="ruleForm"
+      :inline="true"
+      :model="searchMap"
+      class="demo-form-inline"
+      style="margin-top: 20px;"
+    >
+      <el-form-item prop="cardNum">
+        <el-input v-model="searchMap.cardNum" placeholder="会员卡号" style="width: 200px;"></el-input>
+      </el-form-item>
+      <el-form-item prop="name">
+        <el-input v-model="searchMap.name" placeholder="会员名称" style="width: 200px;"></el-input>
+      </el-form-item>
+      <el-form-item prop="payType">
+        <el-select v-model="searchMap.payType" placeholder="支付类型" style="width: 110px;">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="活动时间" prop="birthday">
+        <el-col :span="11">
+          <el-date-picker
+            type="date"
+            placeholder="出生日期"
+            v-model="searchMap.birthday"
+            style="width: 150px"
+          ></el-date-picker>
+        </el-col>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+        <el-button type="success" @click="dialogFormVisible = true">新增</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+
     <!-- 表格数据渲染 
-      stripe:斑马纹 
+      stripe:斑马纹
+      border：边框
     -->
-    <el-table :data="tableData" style="width: 100%" height="600" stripe>
+    <el-table :data="tableData" style="width: 100%" height="600" stripe border>
       <el-table-column fixed type="index" label="序号"></el-table-column>
       <el-table-column fixed prop="cardNum" label="会员卡号" width="90"></el-table-column>
       <el-table-column prop="name" label="会员姓名"></el-table-column>
@@ -31,6 +69,47 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
     ></el-pagination>
+
+    <!-- 对话框 -->
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible" width="500px">
+      <el-form
+        status-icon
+        ref="pojoForm"
+        :model="pojo"
+        label-width="100px"
+        label-position="right"
+        style="width: 400px;"
+      >
+        <el-form-item label="会员卡号" prop="cardNum">
+          <el-input v-model="pojo.cardNum" />
+        </el-form-item>
+        <el-form-item label="会员姓名" prop="name">
+          <el-input v-model="pojo.name" />
+        </el-form-item>
+        <el-form-item label="会员生日" prop="birthday">
+          <el-date-picker v-model="pojo.birthday" type="date" placeholder="请点击选择" />
+        </el-form-item>
+        <el-form-item label="手机号码" prop="phone">
+          <el-input v-model="pojo.phone" />
+        </el-form-item>
+        <el-form-item label="开卡金额" prop="money">
+          <el-input v-model="pojo.money" />
+        </el-form-item>
+        <el-form-item label="可用积分">
+          <el-input v-model="pojo.integral"></el-input>
+        </el-form-item>
+        <el-form-item prop="payType" label="支付类型">
+          <el-select v-model="pojo.payType" placeholder="支付类型" style="width: 110px;">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -44,13 +123,43 @@ export default {
       currentPage: 1, //当前第1页
       pageSize: 10, //一页10条数据
       total: 0,
-      searchMap: {}
+      searchMap: {
+        cardNum: "",
+        name: "",
+        payType: "",
+        birthday: ""
+      },
+      dialogFormVisible: false, //对话框的显示和隐藏,false默认弹窗是隐藏的
+      pojo: {
+        cardNum: "",
+        name: "",
+        birthday: "",
+        phone: "",
+        money: "",
+        integral: "",
+        payType: ""
+      }
     };
   },
 
   components: {},
 
   methods: {
+    //功能：点击查询的时候触发这里
+    onSubmit() {
+      console.log("点击查询按钮了");
+    },
+
+    //功能：新增会员
+    addMember() {
+      console.log("新增会员了");
+    },
+
+    //功能：重置按钮
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+
     //功能:发起请求，获取列表数据渲染
     async getdata() {
       try {
@@ -73,11 +182,13 @@ export default {
       console.log(index, row);
     },
     //功能：翻页的时候触发
-    handleSizeChange() {
+    handleSizeChange(val) {
+      this.pageSize = val; //假设：每页10条；如果在页面切换成20条，下次发生ajax，应该要20条数据
       this.getSearch();
     },
     //功能：当前页发生改变的时候触发
-    handleCurrentChange() {
+    handleCurrentChange(val) {
+      this.currentPage = val;
       this.getSearch();
     },
 
