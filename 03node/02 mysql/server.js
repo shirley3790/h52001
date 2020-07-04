29a264d4471b6dd2d74674c0aff35159f53e12b5
shirley <348475822@qq.com>
2020-07-04 11:13:28 +0800

    更新

--- 03node/02 mysql/server.js
+++ 03node/02 mysql/server.js
@@ -2,13 +2,15 @@
 const express = require('express');
 //导入路由:本质就是一个中间件
 const AllRouter = require('./routers/index');
+//引入配置文件
+const config = require('./config.json');
 
 const app = express();
 
 app.use(express.static('./'));//静态资源服务器
 app.use(AllRouter);//启用主路由
 
-app.listen(3099, () => {
+app.listen(config.port, () => {
     console.log('服务器已开启，请访问3099的端口');
 });
 
